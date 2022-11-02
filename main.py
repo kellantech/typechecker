b3bd8c8e438fabef5334ba246e4a6ffe81ad4b62
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-11-01 20:09:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+anytype = "2dab75841b118164da213ece91a730dae54a3c559b3f637d08577fc13ce22f51bb0f966656bbf738ca0d86075f09702f0fea855fd1eda8a345a7648f9fdc" 
+not_none = "43f9f5a631cb04820e6d0fb22a6bc92f3dd777641530224ee9d09193e5a8cc03ef64e382b1bfa76"
+
 def checktypes(**kwargs):
     def outer(f):
         def check(*args):
@@ -6,17 +9,20 @@ def checktypes(**kwargs):
             try: _ = ty[n]
             except:
               raise ValueError(f"type list needs to be length {len(args)+1}")
-            if isinstance(args[n],ty[n]):pass    
-            else:
-              n2 = n+1
-              if (n2) == 1:
-                suf = 'st'
-              elif (n2) == 2: suf = 'nd'
-              elif (n2) == 3: suf = 'rd'
-              else: suf = 'th'
-              s = f'{n2}{suf} argument to {f.__name__} should be {ty[n].__name__}, not {type(args[n]).__name__}'
-              raise TypeError(s)
+            if ty[n] == anytype: continue
+            if ty[n] == not_none:
+              if args[n] == None:
+                s2 = f'argument number {n+1} to {f.__name__} should not be none'
+                raise TypeError(s2)
+            else:    
+              if isinstance(args[n],ty[n]):pass  
+              else:
+                n2 = n+1
+  
+                s = f'argument number {n2} to {f.__name__} should be {ty[n].__name__}, not {type(args[n]).__name__}'
+                raise TypeError(s)
             
           f(*args)
         return check
     return outer
+
